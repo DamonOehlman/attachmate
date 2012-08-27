75c672c31472820f51bc2961364eba7917efea6a
Damon Oehlman <damon.oehlman@sidelab.com>
2012-08-28 09:37:52 +1000

    Replacing backslashes with forward slashes for windows machines

--- test/upload.js
+++ test/upload.js
@@ -29,6 +29,7 @@ describe('upload tests', function() {
 		it('should be able upload a directory containing a single file', uploadAndCheck('single'));
 		it('should be able to upload a directory with multiple files', uploadAndCheck('multiple'));
 		it('should be able to upload a directory with binary files', uploadAndCheck('binary'));
+		it('should be able to traverse a directory and upload all files', uploadAndCheck('traverse'));
 	});
 
 	describe('short format', function() {
